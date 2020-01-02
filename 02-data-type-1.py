17f281d52982ba39b4a7b3494eb19438a1ab44c3
HYUNSUNG-IM <dna939@naver.com>
2020-01-02 14:35:34 +0900

    add 02-data-type-1.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,20 @@
+#정수
+print (1 + 1)
+#소수 float
+print (1.5 + 2.1)
+#문자 str - string
+print ("hello")
+
+# 문자열 더하기
+# print("hello" + "1")
+
+# 정수 소수 간 변경
+# print(4//2)
+# 정수로 나타내기 int
+# print(int(4/2))
+# print(float(2))
+
+# 문자열 숫자 간 변경
+print("1")
+print(int("1"))
+print(int("1.5"))
